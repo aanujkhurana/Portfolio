5ef66b29ccd8bded2c6aa8022a2e40aaf480aea1
aanujkhurana <aanujkhurana@gmail.com>
2024-03-20 18:47:21 +1000

    FrontendSetup🎉

--- backend/schemaTypes/index.js
+++ backend/schemaTypes/index.js
@@ -1,1 +1,12 @@
-export const schemaTypes = []
+// Code: schemaTypes
+import abouts from './abouts';
+import brands from './brands';
+import contact from './contact';
+import experiences from './experiences';
+import skills from './skills';
+import testimonials from './testimonials';
+import works from './works';
+import workExperience from './workExperience';
+
+export const schemaTypes = [abouts, brands, contact, experiences, skills, testimonials, works, workExperience]
+
