cacf6b72f1a8d814250f2de65761082a9bb4f62d
aanujkhurana <aanujkhurana@gmail.com>
2024-03-23 19:45:07 +1000

    Starting🏎️

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,9 @@ import React from 'react';
 
 import { About, Testimonial, Work, Skills, Footer, Header } from './container';
 import { NavBar } from './components';
+
+import './App.scss';
+
 const App = () => {
   return (
     <div className='app'>
