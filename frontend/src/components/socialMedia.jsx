d38e6eb4070ee20e47e6df8dde401d3819526d4d
aanujkhurana <aanujkhurana@gmail.com>
2024-03-24 21:57:56 +1000

    WRAPPER🌵

---
+++ frontend/src/components/socialMedia.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import { FaGithub,  FaLinkedin, FaFacebook } from "react-icons/fa";
+
+const SocialMedia = () => (
+    <div className="app__social">
+      <div>
+        <FaGithub />
+      </div>
+      <div>
+        <FaLinkedin />
+      </div>
+      <div>
+        <FaFacebook />
+      </div>
+    </div>
+  );
+  
+  export default SocialMedia;
---
+++ frontend/src/wrapper/AppWrap.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import { NavigationDots, socialMedia } from '../components'
+
+const AppWrap = () => {
+  return (
+    <div>AppWrap</div>
+  )
+}
+
+export default AppWrap
