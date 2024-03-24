fba5d7856aeef62f32007dff0a14b40ea0992261
aanujkhurana <aanujkhurana@gmail.com>
2024-03-24 22:01:45 +1000

    WRAPPER🌵

--- frontend/src/wrapper/AppWrap.js
+++ frontend/src/wrapper/AppWrap.js
@@ -1,11 +1,21 @@
-import React from 'react'
+import React from 'react';
+import { NavigationDots, SocialMedia } from '../components';
 
-import { NavigationDots, socialMedia } from '../components'
-
-const AppWrap = () => {
+const AppWrap = (Component, idName, classNames) => function HOC() {
   return (
-    <div>AppWrap</div>
-  )
-}
+    <div id={idName} className={`app__container ${classNames}`}>
+      <SocialMedia />
+      <div className="app__wrapper app__flex">
+        <Component />
 
-export default AppWrap
+        <div className="copyright">
+          <p className="p-text">@2024 AanujKhurana</p>
+          <p className="p-text">All rights reserved</p>
+        </div>
+      </div>
+      <NavigationDots active={idName} />
+    </div>
+  );
+};
+
+export default AppWrap;
