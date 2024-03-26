6a1e0a99da14d867f1fddbfa0c27127c92789cea
aanujkhurana <aanujkhurana@gmail.com>
2024-03-26 16:15:07 +1000

    updates..🌈

--- frontend/src/container/Work/Work.jsx
+++ frontend/src/container/Work/Work.jsx
@@ -49,7 +49,7 @@ const Work = () => {
       <h2 className="head-text">My Creative <span>Portfolio</span></h2>
 
       <div className="app__work-filter">
-        {['UI/UX', 'Web App', 'Mobile App', 'React JS', 'All'].map((item, index) => (
+        {['Web App', 'Mobile App', 'Desktop App', 'Design', 'Other', 'All'].map((item, index) => (
           <div
             key={index}
             onClick={() => handleWorkFilter(item)}
