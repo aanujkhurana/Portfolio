fb0847efe468a11578c99040cec14809f70e70fd
aanujkhurana <aanujkhurana@gmail.com>
2024-03-27 03:52:58 +1000

    updates..🌈

--- frontend/src/container/Work/Work.jsx
+++ frontend/src/container/Work/Work.jsx
@@ -65,23 +65,18 @@ const Work = () => {
         transition={{ duration: 0.5, delayChildren: 0.5 }}
         className="app__work-portfolio"
       >
-        {filterWork.map((work, index) => (
+        {filterWork.sort((a,b) => a._updatedAt.localeCompare(b._updatedAt)).map((work, index) => (
           <div className="app__work-item app__flex" key={index}>
             <div
               className="app__work-img app__flex"
             >
               <img src={urlFor(work.imgUrl)} alt={work.name} />
 
-              <motion.div
-                whileHover={{ opacity: [0, 1] }}
-                transition={{ duration: 0.25, ease: 'easeInOut', staggerChildren: 0.5 }}
-                className="app__work-hover app__flex"
-              >
+              <div className="app__work-hover app__flex">
                 <a href={work.projectLink} target="_blank" rel="noreferrer">
-
                   <motion.div
                     whileInView={{ scale: [0, 1] }}
-                    whileHover={{ scale: [1, 0.90] }}
+                    whileHover={{ scale: [1, 1.3] }}
                     transition={{ duration: 0.25 }}
                     className="app__flex"
                   >
@@ -91,14 +86,14 @@ const Work = () => {
                 <a href={work.codeLink} target="_blank" rel="noreferrer">
                   <motion.div
                     whileInView={{ scale: [0, 1] }}
-                    whileHover={{ scale: [1, 0.90] }}
+                    whileHover={{ scale: [1, 1.3] }}
                     transition={{ duration: 0.25 }}
                     className="app__flex"
                   >
                     <AiFillGithub />
                   </motion.div>
                 </a>
-              </motion.div>
+              </div>
             </div>
 
             <div className="app__work-content app__flex">
