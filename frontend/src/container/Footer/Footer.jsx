4fd1db7bf947b9177d238286d8263565dd63fcb9
aanujkhurana <aanujkhurana@gmail.com>
2024-03-28 13:19:27 +1000

    updates...🌈

--- frontend/src/container/Footer/Footer.jsx
+++ frontend/src/container/Footer/Footer.jsx
@@ -6,22 +6,22 @@ import './Footer.scss'
 const Footer = () => {
   return (
     <footer className="footer">
-      <div className="footer__addr">
-        <img src={images.logoFooter} alt='logo' />
-        <h2>Find me at</h2>
-        <address>
-          Somewhere in; Gold Coast, Australia
-        </address>
-        <motion.div className='footer__action'>
-        <a className="footer__btn" href="mailto:aanujkhurana@gmail.com">Email Me</a>
-        <a className="footer__btn" href="https://raw.githubusercontent.com/aanujkhurana/Portfolio/master/frontend/src/assets/resume/AanujResume.pdf">Download Resume</a>
-        </motion.div>
-      </div>
+      <motion.div 
+        whileInView={{ y: [75, 45, 0], opacity: [0, 0, 1] }}
+        transition={{ duration: 0.5 }}
+      >
+        <div className="footer__addr">
+          <img src={images.logoFooter} alt='logo' />
+          <h2>Find me at</h2>
+          <address>
+            Somewhere in; Gold Coast, Australia
+          </address>
+          <div className='footer__action'>
+          <a className="footer__btn" href="mailto:aanujkhurana@gmail.com">Email Me</a>
+          <a className="footer__btn" href="https://raw.githubusercontent.com/aanujkhurana/Portfolio/master/frontend/src/assets/resume/AanujResume.pdf">Download Resume</a>
+          </div>
+        </div>
 
-      <motion.div
-        whileInView={{ scale: [0.75, 1] }}
-        transition={{ duration: 0.5, ease: 'easeOut' }}
-        >
         <div className="legal">
           <p>&copy; 2024 aanujkhurana. All rights reserved.</p>
           <div className="legal__links">
